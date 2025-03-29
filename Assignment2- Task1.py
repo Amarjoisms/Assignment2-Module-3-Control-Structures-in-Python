6d31b2b72561895ecd33f3ea4ddaaaf0d472db39
Amarjoisms <77470761+Amarjoisms@users.noreply.github.com>
2025-03-29 17:36:07 +0530

    Add files via upload

---
+++ Assignment2- Task1.py
@@ -0,0 +1,6 @@
+a = int(input('Enter a number: '))
+r = a%2
+if (r == 0):
+    print(str(a) + ' is an even number.')
+else:
+    print(str(a) + ' is an odd number.')
---
+++ Assignment2- Task2.py
@@ -0,0 +1,5 @@
+sum = 0
+for i in range(51):
+    a= (sum + i)
+    sum +=i
+    print('The sum of numbers from 1 to '+str(i)  +' is: '+ str(sum))
